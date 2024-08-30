87356c4b06e0de11a19894240632265ec9ba1fd3
mansavigarg <mansavigarg9919@gmail.com>
2024-08-30 19:34:57 +0530

    added atom family

---
+++ Week7.4/5.atom-family/src/App.jsx
@@ -0,0 +1,38 @@
+// Problem 
+// sometimes you need more than one atom for your use case
+// Ex. Creating a TODO application
+
+// Question: create a component that takes a TODO id as a input and renders the todo
+// you need to store the todo in an atom(cant use useState)
+// all the todos can be hardcoded as a variable
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
