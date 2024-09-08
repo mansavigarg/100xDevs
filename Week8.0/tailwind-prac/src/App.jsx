ad43b3773e72ce8370f84c41c3f52d6db943518a
mansavigarg <mansavigarg9919@gmail.com>
2024-09-08 18:05:43 +0530

    tailwind prac

---
+++ Week8.0/tailwind-prac/src/App.jsx
@@ -0,0 +1,12 @@
+import { RevenueCard } from './components/RevenueCard'
+
+function App() {
+
+  return (
+    <div className=' grid grid-cols-3  p-2'>
+      <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+    </div>
+  )
+}
+
+export default App
